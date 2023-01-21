eae6910048dc015355c693133f71f64bbd4fa3bf
Gezeroo <122578684+Gezeroo@users.noreply.github.com>
2023-01-20 22:39:28 -0300

    Add files via upload

---
+++ trab.h
@@ -0,0 +1,24 @@
+#ifndef TRAB_H_INCLUDED
+#define TRAB_H_INCLUDED
+
+typedef struct cliente{
+char usu[31];
+char senha[31];
+}Cliente;
+
+typedef struct lista Lista;
+
+Lista *criar();
+int listaVazia(Lista *l);
+int inserirCli(Lista *l,Cliente it);
+int removerCli(Lista *l,Cliente it);
+int comparaStrg(char a[], char b[]);
+int auth(Lista *l, char a[], char b[]);
+Lista *Inicializar();
+int authADM(char a[], char b[]);
+void mostrar(Lista *l);
+
+int authcard(char c[]);
+
+
+#endif // TRAB_H_INCLUDED
